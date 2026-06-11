7d5d1e2250d0a319af26273f94e08d59eca87b6d
sungjun-p <sungjun95623@naver.com>
2026-06-12 02:17:54 +0900

    init: create travory project

---
+++ src/main/java/com/travory/app/TravoryApplication.java
@@ -0,0 +1,13 @@
+package com.travory.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/travory/app/TravoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travory.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
